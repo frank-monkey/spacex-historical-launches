ad1799e80fabd2e9eb6766f3c3809937aa4a5cd4
Frank <fsacco@andrew.cmu.edu>
2023-07-23 20:12:49 -0400

    Fetch SpaceX launches data from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
 function App() {
+  const [launches, setLaunches] = useState([]);
+
+  const fetchLaunches = async () => {
+    const response = await fetch('https://api.spacexdata.com/v3/launches');
+    const data = await response.json();
+    setLaunches(data);
+  };
+
+  useEffect(() => {
+    fetchLaunches();
+  }, []);
   return (
-    <div className="App">
-      <h1>spacex api</h1>
-    </div>
+      <div>
+        <h1>Historical SpaceX Launches</h1>
+        <p>There were a total of {launches.length} launches 🚀</p>
+      </div>
   );
 }
 
