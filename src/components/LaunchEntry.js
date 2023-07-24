05b39cf444b9320bad7db14a2bc4a930632c1cd9
Frank <fsacco@andrew.cmu.edu>
2023-07-24 13:25:40 -0400

    Added clickable Details field

--- src/components/LaunchEntry.js
+++ src/components/LaunchEntry.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const LaunchEntry= ({ launch }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   const {
     rocket: { rocket_name },
     launch_site: { site_name },
     launch_date_utc,
+    details,
+    success,
+    links: { video_link },
   } = launch;
 
   const formattedDate = new Date(launch_date_utc).toLocaleDateString();
 
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
   return (
-    <div>
+    <div onClick={toggleDetails}>
       <p>Rocket Name: {rocket_name}</p>
       <p>Launch Site: {site_name}</p>
       <p>Date of Launch: {formattedDate}</p>
@@ -20,6 +29,19 @@ const LaunchEntry= ({ launch }) => {
         width={200}
         height={200}
       />
+      {showDetails && (
+        <div>
+          {details && <p>Details: {details}</p>}
+          <p>Success: {success ? 'Yes' : 'No'}</p>
+          {video_link && (
+            <p>
+              <a href={video_link} target="_blank" rel="noopener noreferrer">
+                Watch Launch on YouTube
+              </a>
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
